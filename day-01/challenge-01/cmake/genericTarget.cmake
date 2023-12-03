9e20a6398ed79f4e59c1854bb5647afaee534340
Frozenbitz <peter.heller@fzba.ovh>
2023-12-03 22:28:51 +0100

    bumped c++ standard to c++17
    needed to move the standard configuration up to cpp 17 for regex support

--- day-01/challenge-01/cmake/genericTarget.cmake
+++ day-01/challenge-01/cmake/genericTarget.cmake
@@ -5,8 +5,8 @@ add_library(
 )
 add_library(UTIL::COMPILEOPTIONS ALIAS generic_target)
 
-set(MY_DEBUG_OPTIONS "-O1 -g3 --std=c++11 -DDEBUG")
-set(MY_RELEASE_OPTIONS "-O3 -g1 --std=c++11 -DNDEBUG")
+set(MY_DEBUG_OPTIONS "-O1 -g3 --std=c++17 -DDEBUG")
+set(MY_RELEASE_OPTIONS "-O3 -g1 --std=c++17 -DNDEBUG")
 target_compile_options(generic_target INTERFACE "$<$<CONFIG:DEBUG>:SHELL:${MY_DEBUG_OPTIONS}>")
 target_compile_options(generic_target INTERFACE "$<$<CONFIG:RELEASE>:SHELL:${MY_RELEASE_OPTIONS}>")
 
