71f59da611d9a6fc56b04195ccb7c814d519a12d
Frozenbitz <peter.heller@fzba.ovh>
2023-12-05 22:52:55 +0100

    updated local paths for tests

--- day-01/test/test_calculateCalibrationExamples.cpp
+++ day-01/test/test_calculateCalibrationExamples.cpp
@@ -7,13 +7,13 @@ int main()
     // exception
     // TODO we could add tests for this, to try different settings on streams
  
-    trebuchetCalibration TCalibrate1("/home/peter/Coding/AdventOfCode/day-01/challenge-01/test/test01.txt");
+    trebuchetCalibration TCalibrate1("/home/peter/Coding/AdventOfCode/day-01/test/test01.txt");
     if (TCalibrate1.getCalibrationData() != 281) return -1;
 
-    trebuchetCalibration TCalibrate2("/home/peter/Coding/AdventOfCode/day-01/challenge-01/test/test02.txt");
+    trebuchetCalibration TCalibrate2("/home/peter/Coding/AdventOfCode/day-01/test/test02.txt");
     if (TCalibrate2.getCalibrationData() != 104) return -1;
 
-    trebuchetCalibration TCalibrate3("/home/peter/Coding/AdventOfCode/day-01/challenge-01/test/test03.txt");
+    trebuchetCalibration TCalibrate3("/home/peter/Coding/AdventOfCode/day-01/test/test03.txt");
     if (TCalibrate3.getCalibrationData() != 258) return -1;
     
     return 0; // You can put a 1 here to see later that it would generate an
--- day-01/test/test_trebuchetDigits.cpp
+++ day-01/test/test_trebuchetDigits.cpp
@@ -7,7 +7,7 @@ int main()
     // exception
     // TODO we could add tests for this, to try different settings on streams
  
-    trebuchetCalibration TCalibrate1("/home/peter/Coding/AdventOfCode/day-01/challenge-01/test/testDigits.txt");
+    trebuchetCalibration TCalibrate1("/home/peter/Coding/AdventOfCode/day-01/test/testDigits.txt");
     if (TCalibrate1.getCalibrationData() != 281) return -1;
     
     return 0; // You can put a 1 here to see later that it would generate an
