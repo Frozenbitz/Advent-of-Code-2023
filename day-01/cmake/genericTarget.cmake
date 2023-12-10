f1b37c69519c6ad6906defeeee44058359deefe6
Frozenbitz <peter.heller@fzba.ovh>
2023-12-10 18:08:35 +0100

    updated build options to supress optimization
    changed O1 to O0 to test a few new settings

--- day-01/cmake/genericTarget.cmake
+++ day-01/cmake/genericTarget.cmake
@@ -5,7 +5,7 @@ add_library(
 )
 add_library(UTIL::COMPILEOPTIONS ALIAS generic_target)
 
-set(MY_DEBUG_OPTIONS "-O1 -g3 --std=c++17 -DDEBUG")
+set(MY_DEBUG_OPTIONS "-O0 -g3 --std=c++17 -DDEBUG")
 set(MY_RELEASE_OPTIONS "-O3 -g1 --std=c++17 -DNDEBUG")
 target_compile_options(generic_target INTERFACE "$<$<CONFIG:DEBUG>:SHELL:${MY_DEBUG_OPTIONS}>")
 target_compile_options(generic_target INTERFACE "$<$<CONFIG:RELEASE>:SHELL:${MY_RELEASE_OPTIONS}>")
