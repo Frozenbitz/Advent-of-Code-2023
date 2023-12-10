ff2a484bd91f1a409e0b94e0e370625b6a006de7
Frozenbitz <peter.heller@fzba.ovh>
2023-12-10 22:48:34 +0100

    added builerplate for day 2

---
+++ day-02/src/main.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string inputPath {"./examples/"};
+    std::string inputFile {"/example.txt"};
+    
+
+    return 0;
+}
